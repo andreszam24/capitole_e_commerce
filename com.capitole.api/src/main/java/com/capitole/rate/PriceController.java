1a663211df1543dbec2bad9b20fc54081003796d
Andres Zambrano <andreszambrano18@yahoo.es>
2025-03-11 11:10:13 +0100

    #11 Añadir OpenAPI y Swagger herramientas usadas en API First

--- com.capitole.api/src/main/java/com/capitole/rate/PriceController.java
+++ com.capitole.api/src/main/java/com/capitole/rate/PriceController.java
@@ -1,19 +1,19 @@
 package com.capitole.rate;
 
 import com.capitole.common.QueryBusImpl;
-import com.capitole.rate.dto.FindRateResponseDTO;
+import com.capitole.rate.dto.FindRateResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+
 @RestController
 @RequestMapping("/api")
-public final class PriceController {
+public final class PriceController implements RateApi {
 
     private final QueryBusImpl queryBus;
 
@@ -22,14 +22,22 @@ public final class PriceController {
     }
 
     @GetMapping("rate")
-    public ResponseEntity<FindRateResponseDTO> findRate(
-            @RequestParam long brandId,
-            @RequestParam long productId,
-            @RequestParam @DateTimeFormat LocalDateTime applicationDate) {
+    @Override
+    public ResponseEntity<FindRateResponse> rateGet(
+            Long brandId,
+            Long productId,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime applicationDate) {
 
         var query = new FindRateByBrandIdAndProductIdAndApplicationDateQuery(applicationDate, productId, brandId);
-        var response = queryBus.execute(query);
+        var queryResponse = queryBus.execute(query);
 
-        return ResponseEntity.ok(response);
+        var findRateDTO = new FindRateResponse();
+        findRateDTO.setPrice(queryResponse.price());
+        findRateDTO.setBrandId(queryResponse.brandId());
+        findRateDTO.priceList(queryResponse.priceList());
+        findRateDTO.setProductId(queryResponse.productId());
+        findRateDTO.setStartDate(queryResponse.startDate());
+        findRateDTO.setEndDate(queryResponse.endDate());
+        return ResponseEntity.ok(findRateDTO);
     }
 }
--- com.capitole.application/src/main/java/com/capitole/rate/dto/FindRateQueryResponse.java
+++ com.capitole.application/src/main/java/com/capitole/rate/dto/FindRateQueryResponse.java
@@ -1,9 +1,8 @@
 package com.capitole.rate.dto;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record FindRateResponseDTO(
+public record FindRateQueryResponse(
 
         long productId,
         long brandId,
